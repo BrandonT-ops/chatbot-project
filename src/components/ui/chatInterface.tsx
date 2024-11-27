5633be6af6a709ad8b5f55f3f7977a6b3d13b2aa
Brandon Tapiba <brandontapiba@gmail.com>
2024-11-27 19:02:06 +0100

    small effects

--- src/components/ui/chatInterface.tsx
+++ src/components/ui/chatInterface.tsx
@@ -9,12 +9,13 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const ChatInterface: React.FC = () => {
   const [input, setInput] = useState("");
   const [pendingFiles, setPendingFiles] = useState<File[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const { messages, addMessage , searchResults} = useChatStore();
+  const { messages, addMessage, searchResults } = useChatStore();
 
   const triggerFileInput = () => {
     fileInputRef.current?.click();
@@ -95,14 +96,20 @@ const ChatInterface: React.FC = () => {
         {msg.images && msg.images.length > 0 && (
           <div className="flex flex-wrap gap-2">
             {msg.images.map((img, index) => (
-              <Image
+              <motion.div
                 key={index}
-                src={img}
-                alt={`Uploaded image ${index + 1}`}
-                width={100}
-                height={100}
-                className="object-cover rounded-lg"
-              />
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Image
+                  src={img}
+                  alt={`Uploaded image ${index + 1}`}
+                  width={100}
+                  height={100}
+                  className="object-cover rounded-lg"
+                />
+              </motion.div>
             ))}
           </div>
         )}
@@ -111,13 +118,16 @@ const ChatInterface: React.FC = () => {
         {msg.files && msg.files.length > 0 && (
           <div className="space-y-1">
             {msg.files.map((file, index) => (
-              <div
+              <motion.div
                 key={index}
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
                 className="flex items-center space-x-2 bg-gray-100 p-2 rounded-lg"
               >
                 <DocumentIcon className="h-5 w-5 text-gray-500" />
                 <span className="text-sm">{file.name}</span>
-              </div>
+              </motion.div>
             ))}
           </div>
         )}
@@ -137,8 +147,11 @@ const ChatInterface: React.FC = () => {
             </h2>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
               {[1, 2, 3, 4].map((_, index) => (
-                <div 
-                  key={index} 
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.5 }}
                   className="bg-white border rounded-lg p-4 shadow-sm animate-pulse"
                 >
                   <div className="h-48 bg-gray-300 rounded-t-lg mb-4"></div>
@@ -147,14 +160,14 @@ const ChatInterface: React.FC = () => {
                     <div className="h-4 bg-gray-300 rounded w-1/2"></div>
                     <div className="h-4 bg-gray-300 rounded w-1/3"></div>
                   </div>
-                </div>
+                </motion.div>
               ))}
             </div>
           </div>
         </div>
       );
     }
-  
+
     return (
       <div className="space-y-4">
         <div className="bg-blue-50 p-4 rounded-lg">
@@ -166,15 +179,18 @@ const ChatInterface: React.FC = () => {
           ) : (
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
               {searchResults.results.map((product, index) => (
-                <div 
-                  key={index} 
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.5 }}
                   className="bg-white border rounded-lg p-4 shadow-sm hover:shadow-md transition"
                 >
-                  <Image 
-                    src={product.image_url || "/api/placeholder/200/200"} 
-                    alt={product.name} 
-                    width={200} 
-                    height={200} 
+                  <Image
+                    src={product.image_url || "/api/placeholder/200/200"}
+                    alt={product.name}
+                    width={200}
+                    height={200}
                     className="w-full h-48 object-cover rounded-t-lg"
                     unoptimized
                   />
@@ -182,13 +198,13 @@ const ChatInterface: React.FC = () => {
                     <h3 className="font-semibold text-gray-800">{product.name}</h3>
                     <p className="text-gray-600">{product.price} FCFA</p>
                     <p className={`text-sm ${
-                      product.disponibilite === 'Out of Stock !' 
-                        ? 'text-red-500' 
+                      product.disponibilite === 'Out of Stock !'
+                        ? 'text-red-500'
                         : 'text-green-500'
                     }`}>
                       {product.disponibilite}
                     </p>
-                    <button 
+                    <button
                       className="mt-2 w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
                       onClick={() => {
                         addMessage({
@@ -200,7 +216,7 @@ const ChatInterface: React.FC = () => {
                       View Details
                     </button>
                   </div>
-                </div>
+                </motion.div>
               ))}
             </div>
           )}
@@ -217,102 +233,118 @@ const ChatInterface: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col min-h-screen pt-16 bg-white">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="flex flex-col min-h-screen pt-16 bg-white"
+    >
       <div className="w-full max-w-5xl mx-auto flex-grow flex flex-col mt-8">
         {/* Container with centered content */}
         <div className="flex-grow bg-white rounded-lg p-6">
           {/* Title */}
-          <h1 className="text-3xl font-bold text-gray-800 mb-6">
-            {searchResults 
-              ? `Search Results for "${searchResults.query}"` 
+          <motion.h1
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-3xl font-bold text-gray-800 mb-6"
+          >
+            {searchResults
+              ? `Search Results for "${searchResults.query}"`
               : "Need help finding a product?"
             }
-          </h1>
+          </motion.h1>
 
           {/* Chat Messages Container */}
           <div className="flex-grow overflow-y-auto mb-6 space-y-4">
-
-          {searchResults ? (
+            {searchResults ? (
               renderSearchResults()
             ) : (
               <>
-            {/* Default AI Initial Message */}
-            <div className="flex items-start space-x-3">
-              <Image
-                width={100}
-                height={100}
-                alt="Pp"
-                src="assets/chat_icon.svg"
-                className="size-8 rounded-full"
-              />
-
-              <div className="bg-white px-1 rounded-lg flex-grow ">
-                <div className="flex items-center mb-2">
-                  <span className="font-semibold text-gray-800 mr-2">
-                    Maguida
-                  </span>
-                </div>
-                <p className="text-gray-600 text-sm text-justify max-w-4xl">
-                  Hi there! I&apos;m Maguida, your personal shopping assistant.
-                  I can help you find the perfect product! Just tell me what
-                  you&apos;re looking for.
-                </p>
-              </div>
-            </div>
-
-            {/* Existing Messages */}
-            {messages.map((msg, index) => (
-              <div
-                key={index}
-                className={`flex  ${
-                  msg.role === "user" ? "justify-start" : "justify-start"
-                }`}
-              >
-                {msg.role === "user" ? (
-                  // User message
-                  <div className="flex items-start space-x-3 my-3">
-                    <Image
-                      width={100}
-                      height={100}
-                      alt="Pp"
-                      src="assets/profile_picture.svg"
-                      className="size-8 rounded-full"
-                    />
-                    <div className="bg-white px-1 rounded-lg flex-grow">
-                      <div className="flex items-center mb-2">
-                        <span className="font-semibold text-gray-800 mr-2">
-                          User
-                        </span>
-                      </div>
-                      <div className="text-gray-600 text-sm text-justify max-w-4xl">
-                        {renderMessageContent(msg)}
-                      </div>
+                {/* Default AI Initial Message */}
+                <motion.div
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                  className="flex items-start space-x-3"
+                >
+                  <Image
+                    width={100}
+                    height={100}
+                    alt="Pp"
+                    src="assets/chat_icon.svg"
+                    className="size-8 rounded-full"
+                  />
+                  <div className="bg-white px-1 rounded-lg flex-grow ">
+                    <div className="flex items-center mb-2">
+                      <span className="font-semibold text-gray-800 mr-2">
+                        Maguida
+                      </span>
                     </div>
+                    <p className="text-gray-600 text-sm text-justify max-w-4xl">
+                      Hi there! I&apos;m Maguida, your personal shopping assistant.
+                      I can help you find the perfect product! Just tell me what
+                      you&apos;re looking for.
+                    </p>
                   </div>
-                ) : (
-                  // AI message
-                  <div className="flex items-start space-x-3">
-                    <Image
-                      width={100}
-                      height={100}
-                      alt="Maguida Chat Icon"
-                      src="assets/chat_icon.svg"
-                      className="size-8 rounded-full"
-                    />
-                    <div className="bg-white px-1 rounded-lg flex-grow">
-                      <div className="flex items-center mb-2">
-                        <span className="font-semibold text-gray-800 mr-2">
-                          Maguida
-                        </span>
+                </motion.div>
+
+                {/* Existing Messages */}
+                {messages.map((msg, index) => (
+                  <motion.div
+                    key={index}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className={`flex ${
+                      msg.role === "user" ? "justify-start" : "justify-start"
+                    }`}
+                  >
+                    {msg.role === "user" ? (
+                      // User message
+                      <div className="flex items-start space-x-3 my-3">
+                        <Image
+                          width={100}
+                          height={100}
+                          alt="Pp"
+                          src="assets/profile_picture.svg"
+                          className="size-8 rounded-full"
+                        />
+                        <div className="bg-white px-1 rounded-lg flex-grow">
+                          <div className="flex items-center mb-2">
+                            <span className="font-semibold text-gray-800 mr-2">
+                              User
+                            </span>
+                          </div>
+                          <div className="text-gray-600 text-sm text-justify max-w-4xl">
+                            {renderMessageContent(msg)}
+                          </div>
+                        </div>
                       </div>
-                      <div className="text-gray-600 text-sm text-justify max-w-4xl">
-                        {renderMessageContent(msg)}
+                    ) : (
+                      // AI message
+                      <div className="flex items-start space-x-3">
+                        <Image
+                          width={100}
+                          height={100}
+                          alt="Maguida Chat Icon"
+                          src="assets/chat_icon.svg"
+                          className="size-8 rounded-full"
+                        />
+                        <div className="bg-white px-1 rounded-lg flex-grow">
+                          <div className="flex items-center mb-2">
+                            <span className="font-semibold text-gray-800 mr-2">
+                              Maguida
+                            </span>
+                          </div>
+                          <div className="text-gray-600 text-sm text-justify max-w-4xl">
+                            {renderMessageContent(msg)}
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-            ))}
+                    )}
+                  </motion.div>
+                ))}
               </>
             )}
           </div>
@@ -321,7 +353,13 @@ const ChatInterface: React.FC = () => {
           {pendingFiles.length > 0 && (
             <div className="mb-4 flex flex-wrap gap-2">
               {pendingFiles.map((file, index) => (
-                <div key={index} className="relative">
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.5 }}
+                  className="relative"
+                >
                   {file.type.startsWith("image/") ? (
                     <Image
                       src={URL.createObjectURL(file)}
@@ -342,57 +380,59 @@ const ChatInterface: React.FC = () => {
                   >
                     <XMarkIcon className="h-4 w-4" />
                   </button>
-                </div>
+                </motion.div>
               ))}
             </div>
           )}
 
           {/* Input Section */}
           {searchResults ? (
-              //renderSearchResults()
-              writeNothing()
-            ) : (
-              <>
-          <div className="flex items-center space-x-2 ">
-            {/* Hidden file input */}
-            <input
-              type="file"
-              ref={fileInputRef}
-              multiple
-              onChange={handleFileUpload}
-              className="hidden"
-              accept="image/*,application/pdf,.doc,.docx,.txt,.png,.svg,.jpg,.jpeg"
-            />
-
-            {/* Text input */}
-            <input
-              type="text"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
-              className="text-gray-900 text-sm flex-grow p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
-              placeholder="Chat with me..."
-            />
-
-            {/* File upload button */}
-            <button
-              onClick={triggerFileInput}
-              className="bg-gray-200 p-3 rounded-lg hover:bg-gray-300 transition"
+            writeNothing()
+          ) : (
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              className="flex items-center space-x-2"
             >
-              <PaperClipIcon className="h-5 w-5 text-gray-700" />
-            </button>
+              {/* Hidden file input */}
+              <input
+                type="file"
+                ref={fileInputRef}
+                multiple
+                onChange={handleFileUpload}
+                className="hidden"
+                accept="image/*,application/pdf,.doc,.docx,.txt,.png,.svg,.jpg,.jpeg"
+              />
 
-            {/* Send button */}
-            <button
-              onClick={handleSendMessage}
-              className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition flex items-center"
-            >
-              <PaperAirplaneIcon className="h-5 w-5 mr-2" />
-              Send
-            </button>
-          </div>
-          </>
-            )}
+              {/* Text input */}
+              <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
+                className="text-gray-900 text-sm flex-grow p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-500"
+                placeholder="Chat with me..."
+              />
+
+              {/* File upload button */}
+              <button
+                onClick={triggerFileInput}
+                className="bg-gray-200 p-3 rounded-lg hover:bg-gray-300 transition"
+              >
+                <PaperClipIcon className="h-5 w-5 text-gray-700" />
+              </button>
+
+              {/* Send button */}
+              <button
+                onClick={handleSendMessage}
+                className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition flex items-center"
+              >
+                <PaperAirplaneIcon className="h-5 w-5 mr-2" />
+                Send
+              </button>
+            </motion.div>
+          )}
         </div>
 
         {/* Copyright */}
@@ -400,7 +440,7 @@ const ChatInterface: React.FC = () => {
           © {new Date().getFullYear()} Richenel&apos;s AI Agency. All rights reserved
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
